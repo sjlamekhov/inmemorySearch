9c2d84ff33442b5aa7345d4f16229870b24ded0c
sjlamekhov <sjlamekhov@gmail.com>
2020-03-03 06:47:09 -0500

    PlainSearchRequestOptimizer object usage optimizations

--- src/main/java/search/optimizer/PlainSearchRequestOptimizer.java
+++ src/main/java/search/optimizer/PlainSearchRequestOptimizer.java
@@ -1,5 +1,7 @@
 package search.optimizer;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import search.ConditionType;
 import search.request.SearchRequest;
 
@@ -11,38 +13,51 @@ import static java.util.stream.Collectors.toSet;
 public class PlainSearchRequestOptimizer implements SearchRequestOptimizer {
 
     public SearchRequest optimize(SearchRequest input) {
+        return optimizeInternal(input).getLeft();
+    }
+
+    private ImmutablePair<SearchRequest, Boolean> optimizeInternal(SearchRequest input) {
         Set<SearchRequest> andRequests = input.getAndRequests();
         Set<SearchRequest> orRequests = input.getOrRequests();
         boolean processAndRequests = !andRequests.isEmpty();    //AND requests are in priority
-        List<SearchRequest> optimizedAndRequests = new ArrayList<>(andRequests.size());
-        Set<SearchRequest> optimizedOrRequests = new HashSet<>(orRequests.size());
+        List<SearchRequest> optimizedAndRequests = null;
+        List<SearchRequest> optimizedOrRequests = null;
+        boolean changesDetected = false;
         if (processAndRequests) {
+            optimizedAndRequests = new ArrayList<>(andRequests.size());
             for (SearchRequest searchRequest : andRequests) {
-                SearchRequest optimized = optimize(searchRequest);
-                if (null == optimized) {
-                    return null;
+                Pair<SearchRequest, Boolean> optimized = optimizeInternal(searchRequest);
+                if (null == optimized.getLeft()) {
+                    return new ImmutablePair<>(null, true);
                 }
-                optimizedAndRequests.add(optimized);
+                changesDetected |= optimized.getRight();
+                optimizedAndRequests.add(optimized.getLeft());
             }
             if (!checkRequestCompatibility(input, optimizedAndRequests)) {
-                return null;
+                return new ImmutablePair<>(null, true);
             }
         } else {
+            optimizedOrRequests = new ArrayList<>(orRequests.size());
             for (SearchRequest searchRequest : orRequests) {
-                SearchRequest optimized = optimize(searchRequest);
-                if (null == optimized) {
-                    return null;
+                Pair<SearchRequest, Boolean> optimized = optimizeInternal(searchRequest);
+                if (null == optimized.getLeft()) {
+                    return new ImmutablePair<>(null, true);
                 }
-                optimizedOrRequests.add(optimized);
+                changesDetected |= !optimized.getRight();
+                optimizedOrRequests.add(optimized.getLeft());
             }
         }
-        return SearchRequest.Builder.newInstance()
-                .setConditionType(input.getConditionType())
-                .setAttributeToSearch(input.getAttributeToSearch())
-                .setValueToSearch(input.getValueToSearch())
-                .and(optimizedAndRequests)
-                .or(optimizedOrRequests)
-                .build();
+        if (!changesDetected) {
+            return new ImmutablePair<>(input, false);
+        } else {
+            return new ImmutablePair<>(SearchRequest.Builder.newInstance()
+                    .setConditionType(input.getConditionType())
+                    .setAttributeToSearch(input.getAttributeToSearch())
+                    .setValueToSearch(input.getValueToSearch())
+                    .and(processAndRequests ? optimizedAndRequests : andRequests)
+                    .or(!processAndRequests ? optimizedOrRequests : orRequests)
+                    .build(), true);
+        }
     }
 
     private static boolean checkRequestCompatibility(SearchRequest input, Collection<SearchRequest> optimizedAndRequests) {
@@ -63,7 +78,7 @@ public class PlainSearchRequestOptimizer implements SearchRequestOptimizer {
             if (null != eqRequests && eqRequests.size() > 1) {
                 Set<String> values = eqRequests.stream().map(SearchRequest::getValueToSearch).collect(toSet());
                 if (values.size() > 1) {
-                    return false;   //there are search requests with differente EQ values on same level
+                    return false;   //there are search requests with different EQ values on same level
                 }
             }
             //ConditionType.LT && ConditionType.GT
