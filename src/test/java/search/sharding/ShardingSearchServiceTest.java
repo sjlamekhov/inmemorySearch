04df40f232d9108aeacd889b0b77c76db2e344f1
sjlamekhov <sjlamekhov@gmail.com>
2020-03-10 02:08:52 -0400

    sharding test and fixes

--- src/test/java/search/sharding/ShardingSearchServiceTest.java
+++ src/test/java/search/sharding/ShardingSearchServiceTest.java
@@ -3,20 +3,52 @@ package search.sharding;
 import objects.Document;
 import objects.DocumentUri;
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
 import search.AbstractSearchServiceTest;
+import search.ConditionType;
 import search.SearchService;
 import search.inmemory.InMemorySearchService;
+import search.request.SearchRequest;
 import search.sharded.ShardedSearchService;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShardingSearchServiceTest extends AbstractSearchServiceTest {
 
     private ShardingServiceMock<DocumentUri, Document> shardingServiceMock;
+    private InMemorySearchService<DocumentUri, Document> localSearchService;
 
     @Override
     protected SearchService<DocumentUri, Document> getSearchService() {
+        localSearchService = new InMemorySearchService<>();
         shardingServiceMock = new ShardingServiceMock<>();
-        return new ShardedSearchService<>(new InMemorySearchService<>(),
-                shardingServiceMock);
+        return new ShardedSearchService<>(localSearchService, shardingServiceMock);
+    }
+
+    @Test
+    public void shardingTest() {
+        SearchRequest searchRequest = SearchRequest.Builder.newInstance()
+                .setAttributeToSearch("attribute")
+                .setConditionType(ConditionType.EQ)
+                .setValueToSearch("value")
+                .build();
+
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("attribute", "value");
+        Document localDocument = new Document(new DocumentUri(tenantId), attributes);
+        Document forShardingDocument = new Document(new DocumentUri(tenantId), attributes);
+        Assert.assertNotEquals(localDocument.getUri(), forShardingDocument.getUri());
+
+        localSearchService.addObjectToIndex(localDocument);
+        shardingServiceMock.addSearchRequestAndResult(searchRequest, forShardingDocument);
+
+        Collection<DocumentUri> searchResult = searchService.search(tenantId, searchRequest);
+        Assert.assertEquals(2, searchResult.size());
+        Assert.assertTrue(searchResult.containsAll(Arrays.asList(localDocument.getUri(), forShardingDocument.getUri())));
     }
 
     @After
--- src/test/java/search/sharding/ShardingServiceMock.java
+++ src/test/java/search/sharding/ShardingServiceMock.java
@@ -11,34 +11,39 @@ import java.util.*;
 
 public class ShardingServiceMock<U extends AbstractObjectUri, T extends AbstractObject> extends ShardingService<U, T>  {
 
-    private final Map<SearchRequest, Set<DocumentUri>> searchRequestAndDocuments;
+    private final Map<SearchRequest, Set<U>> searchRequestAndDocuments;
 
     public ShardingServiceMock() {
         super(Collections.emptyList(), null);
         this.searchRequestAndDocuments = new HashMap<>();
     }
 
-    public void addSearchRequestAndResult(SearchRequest searchRequest, Document document) {
-        if (null == document) {
+    public void addSearchRequestAndResult(SearchRequest searchRequest, T object) {
+        if (null == object) {
             return;
         }
         searchRequestAndDocuments
                 .computeIfAbsent(searchRequest, i -> new HashSet<>())
-                .add(document.getUri());
+                .add((U) object.getUri());
     }
 
     @Override
     public Map<SearchRequest, Collection<U>> executeShardedRequest(SearchRequest searchRequest) {
-        return Collections.emptyMap();
+        return executeShardedRequests(Collections.singleton(searchRequest));
     }
 
     @Override
-    public Map<SearchRequest, Collection<U>> executeShardedRequests(Collection<SearchRequest> searchRequest) {
-        return Collections.emptyMap();
+    public Map<SearchRequest, Collection<U>> executeShardedRequests(Collection<SearchRequest> searchRequests) {
+        Map<SearchRequest, Collection<U>> result = new HashMap<>();
+        for (SearchRequest searchRequest : searchRequests) {
+            result.put(searchRequest, searchRequestAndDocuments.getOrDefault(searchRequest, Collections.emptySet()));
+        }
+        return result;
     }
 
     @Override
     public void removeObjectFromIndex(T object) {
+        searchRequestAndDocuments.values().forEach(i -> i.remove(object.getUri()));
     }
 
     public void dropIndexes() {
